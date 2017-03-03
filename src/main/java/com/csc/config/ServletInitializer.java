62ef104ad082bd9e4d86eb5c16b921979cdaa620
hailt <hailt09111994@gmail.com>
2017-03-03 16:40:22 -0500

    commit

---
+++ src/main/java/com/csc/config/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.csc.config;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EInvoiceApplication.class);
+	}
+
+}
---
+++ src/main/java/com/csc/controller/NotifyController.java
@@ -0,0 +1,8 @@
+package com.csc.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotifyController {
+
+}
