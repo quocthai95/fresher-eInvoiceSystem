2e7dc6b3c05e259db58c5b716208942e421baafc
hailt <hailt09111994@gmail.com>
2017-03-14 18:37:15 -0400

    src\main\resources\templates\register.html src\main\resources\static\js\app.js src\main\resources\static\js\controller\registerController.js src\main\resources\static\js\controller\signup.js src\main\resources\static\views\signup.html src\main\resources\templates\index.html

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', [ 'ngRoute' ]);
+var app = angular.module('myApp', [ 'ngRoute', 'ng-sweet-alert']);
 app.config(function($routeProvider) {
 	$routeProvider.when("/", {
 		templateUrl : "/EInvoice/views/home.html",
