128cd8882c741606c344c61688f2a70874fa4cae
hailt <hailt09111994@gmail.com>
2017-03-16 14:10:37 -0400

    src\main\java\csc\config\DataSeedingListener.java src\main\java\csc\controllers\InvoiceController.java src\main\java\csc\models\Company.java src\main\java\csc\models\Invoice.java src\main\java\csc\models\TypeInvoice.java src\main\java\csc\repository\TypeInvoiceRepository.java

--- src/main/java/csc/repository/TypeInvoiceRepository.java
+++ src/main/java/csc/repository/TypeInvoiceRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import csc.models.TypeInvoice;
+import java.lang.String;
+import java.util.List;
 
 @Repository
 public interface TypeInvoiceRepository extends CrudRepository<TypeInvoice, Integer> {
