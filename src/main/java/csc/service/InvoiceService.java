7647ff1b98e53f7b2b0900a4589e01a059f75e32
Le Thanh Hai <hailt09111994@gmail.com>
2017-03-19 21:42:50 +0700

    improve code function report

--- src/main/java/csc/service/InvoiceService.java
+++ src/main/java/csc/service/InvoiceService.java
@@ -1,5 +1,7 @@
 package csc.service;
 
+import java.util.Date;
+
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
@@ -9,26 +11,25 @@ import csc.models.Customer;
 import csc.models.Invoice;
 
 public interface InvoiceService {
-	
+
 	Invoice findById(long id);
-		
-	
-	@CacheEvict(value="report", allEntries=true)
+
+	@CacheEvict(value = "report", allEntries = true)
 	void saveInvoice(Invoice invoice);
-	
-	@CacheEvict(value="report", allEntries=true)
+
+	@CacheEvict(value = "report", allEntries = true)
 	void updateInvoice(Invoice invoice);
-	
-	@CacheEvict(value="report", allEntries=true)
+
+	@CacheEvict(value = "report", allEntries = true)
 	void deleteInvoiceById(long id);
 
 	@Cacheable("report")
 	Page<Invoice> findAllInvoice(Pageable pageable);
-	
+
 	@Cacheable("report")
 	Page<Invoice> findByIdCustomer(Customer idcustomer, Pageable pageable);
-	
+
 	@Cacheable("report")
-	Page<Invoice> getListReport(String idCus, String dateStart, String dateEnd, int page, int pageSize);
+	Page<Invoice> getListReport(Customer idCus, Date dateStart, Date dateEnd, int page, int pageSize);
 
 }
